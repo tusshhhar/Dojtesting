837e93e503c1296802fe96713c36a089e9136f93
Tushar Vishnoi <tusharvishnoi1998@gmail.com>
2025-03-08 16:51:06 +0530

    Updated

--- DojAppProject/src/test/java/D_Click_on_filter/Click_on_short.java
+++ DojAppProject/src/test/java/D_Click_on_filter/Click_on_short.java
@@ -1,7 +1,7 @@
 package D_Click_on_filter;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 
 public class Click_on_short {
@@ -16,14 +16,22 @@ public class Click_on_short {
 				   
 				   	        JavascriptExecutor js = (JavascriptExecutor) driver;
 				   	        
+				   	        
 				   	        // Scroll down by 1000 pixels
+				   	     Thread.sleep(3000);
 				   	        js.executeScript("window.scrollBy(0,1000)");
 				   	        
-				   	     Thread.sleep(3000);
+				   	   
 				   	  // Click on sort
-				   	     
+				   	   Thread.sleep(3000);
+				   	  driver.findElement(By.xpath("//span[@class='text-sm font-medium text-[#575757]']")).click();
+				   
+				   	 // Click on High to low
+				      Thread.sleep(3000);
+				   	  driver.findElement(By.xpath("//li[normalize-space()='Price: High to Low']")).click();
+				   
 				  
-			
+				  
 			}
 
 		}
